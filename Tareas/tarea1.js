23d45db2c0cf90fbea2ce7c8614f01c118b6ca5c
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2022-10-29 17:01:43 -0300

    Crea la parte básica de la pagina web

--- Tareas/tarea1.js
+++ Tareas/tarea1.js
@@ -5,3 +5,22 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, 
             borrando los inputs ya creados (investigar cómo en MDN).
 */
+function crearInputConLabel(i) {
+    const nuevoLabel = document.createElement('label');
+    nuevoLabel.innerText = `Ingrese la edad del integrante ${i+1} `;
+    const nuevoInput = document.createElement('input');
+    nuevoInput.setAttribute('type', 'number');
+    nuevoInput.className = 'edad';
+    const nuevoDiv = document.createElement('div');
+    nuevoDiv.appendChild(nuevoLabel);
+    nuevoDiv.appendChild(nuevoInput);
+    return nuevoDiv;
+}
+
+const $botonContinuar = document.querySelector('#continuar');
+$botonContinuar.onclick = function() {
+    for (let i = 0; i < Number(document.querySelector('#cantidad-integrantes').value); i++) {
+        document.querySelector('div').appendChild(crearInputConLabel(i));
+    }
+    return false;
+}
