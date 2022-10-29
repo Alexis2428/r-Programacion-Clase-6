3cedaa59406a0980fb5581777fc6042f3b78c6f9
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2022-10-29 17:08:41 -0300

    Agrega funcionalidad a la pagina

--- Tareas/tarea1.js
+++ Tareas/tarea1.js
@@ -5,6 +5,55 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, 
             borrando los inputs ya creados (investigar cómo en MDN).
 */
+
+function obtenerMayorEdad(edades) {
+    let mayorEdad = edades[0];
+    for (let i = 1; i < edades.length; i++) {
+        if (mayorEdad < edades[i]) {
+            mayorEdad = edades[i];
+        }
+    }
+    return mayorEdad;
+}
+function obtenerMenorEdad(edades) {
+    let menorEdad = edades[0];
+    for (let i = 1; i < edades.length; i++) {
+        if (menorEdad > edades[i]) {
+            menorEdad = edades[i];
+        }
+    }
+    return menorEdad;
+}
+function calcularPromedio(edades) {
+    let sumaTotal = 0;
+    for (let i = 0; i < edades.length; i++) {
+        sumaTotal += edades[i];
+    }
+    return (sumaTotal / edades.length).toFixed(2);
+}
+function obtenerEdades() {
+    const listaEdades = document.querySelectorAll('.edad');
+    const edades = [];
+    listaEdades.forEach(edad => {
+        edades.push(Number(edad.value));
+    });
+    return edades;
+}
+
+function crearBotonResetear() {
+    const botonResetear = document.createElement('button');
+    botonResetear.setAttribute('type', 'button');
+    botonResetear.setAttribute('id', 'resetear');
+    botonResetear.innerText = 'Volver a empezar';
+    return botonResetear;
+}
+function crearBotonCalcular() {
+    const botonCalcular = document.createElement('button');
+    botonCalcular.setAttribute('type', 'button');
+    botonCalcular.setAttribute('id', 'calcular');
+    botonCalcular.innerText = 'Calcular';
+    return botonCalcular;
+}
 function crearInputConLabel(i) {
     const nuevoLabel = document.createElement('label');
     nuevoLabel.innerText = `Ingrese la edad del integrante ${i+1} `;
@@ -12,6 +61,7 @@ function crearInputConLabel(i) {
     nuevoInput.setAttribute('type', 'number');
     nuevoInput.className = 'edad';
     const nuevoDiv = document.createElement('div');
+    nuevoDiv.className = 'campoIntegrantes';
     nuevoDiv.appendChild(nuevoLabel);
     nuevoDiv.appendChild(nuevoInput);
     return nuevoDiv;
@@ -23,4 +73,21 @@ $botonContinuar.onclick = function() {
         document.querySelector('div').appendChild(crearInputConLabel(i));
     }
     return false;
+}
+document.querySelector('#boton').appendChild(crearBotonCalcular());
+const $botonCalcular = document.querySelector('#calcular');
+$botonCalcular.onclick = function() {
+    const edades = obtenerEdades();
+    document.querySelector('#mayor-edad').innerText += obtenerMayorEdad(edades);
+    document.querySelector('#menor-edad').innerText += obtenerMenorEdad(edades);
+    document.querySelector('#promedio').innerText += calcularPromedio(edades);
+    return false;
+}
+document.querySelector('body').appendChild(crearBotonResetear());
+const $botonResetear = document.querySelector('#resetear');
+$botonResetear.onclick = function() {
+    document.querySelector('#mayor-edad').innerText = 'La mayor edad del grupo familiar es ...';
+    document.querySelector('#menor-edad').innerText = 'La menor edad del grupo familiar es ...';
+    document.querySelector('#promedio').innerText = 'El promedio de edades del grupo familiar es ...';
+    return;
 }
